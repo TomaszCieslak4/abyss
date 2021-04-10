0486fbef21568cde563f1ba307da932b36f20917
Julian de Rushe <julian.derushe@mail.utoronto.ca>
2021-04-10 01:47:22 -0400

    Start stat display

--- abyss/src/pages/Game/Game.tsx
+++ abyss/src/pages/Game/Game.tsx
@@ -3,10 +3,40 @@ import { Component } from "react";
 import "../../App.css";
 import { PORT } from "../../config";
 
-class Game extends Component {
+interface MyState {
+  health: number,
+  score: number,
+  ammo: number,
+}
+
+class Game extends Component<{}, MyState> {
   constructor(props: any) {
     super(props);
-    // Test();
+    this.state = {
+      health: 0,
+      score: 0,
+      ammo: 0,
+    };
+    this.updateScore = this.updateScore.bind(this);
+    this.updateHealth = this.updateHealth.bind(this);
+    this.updateAmmo = this.updateAmmo.bind(this);
+  }
+  updateScore(value: number) {
+    this.setState({
+      score: value,
+    });
+  }
+
+  updateHealth(value: number) {
+    this.setState({
+      health: value,
+    });
+  }
+
+  updateAmmo(value: number) {
+    this.setState({
+      ammo: value,
+    });
   }
 
   componentDidMount() {
@@ -20,20 +50,24 @@ class Game extends Component {
   }
 
   render() {
-    return (<canvas
-      id="canvas"
-      onContextMenu={event => event.preventDefault()}
-      tabIndex={-1}
-      style={{
-        border: "none",
-        background: "black",
-        width: "100%",
-        height: "100%",
-        padding: "0",
-        margin: "0",
-        boxSizing: "border-box",
-      }}>
-    </canvas>
+    return (
+      <div>
+
+        <canvas
+          id="canvas"
+          onContextMenu={event => event.preventDefault()}
+          tabIndex={-1}
+          style={{
+            border: "none",
+            background: "black",
+            width: "100%",
+            height: "100%",
+            padding: "0",
+            margin: "0",
+            boxSizing: "border-box",
+          }}>
+        </canvas>
+      </div>
     );
   }
 }
